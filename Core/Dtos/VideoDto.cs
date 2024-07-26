b4b6674b9cf8b352ee8e95752475606d7e36ba8a
38098 <38098@DESKTOP-77BM2BU>
2024-07-26 21:13:22 +0300

    #7 -  Add profile

--- Core/Dtos/VideoDto.cs
+++ Core/Dtos/VideoDto.cs
@@ -3,7 +3,7 @@ namespace Core.Dtos
 {
     public class VideoDto
     {
-        public int? Id { get; set; }
+        public int? Id { get; set; } 
         public int UserId { get; set; }
         public string UserNickname { get; set; }
         public string Title { get; set; }
